8c70131edb1ee0c8f2287fa1aad671f931be2939
fsiqueirag <fabrizio.siqueira@hotmail.com>
2020-11-05 17:48:26 -0300

    updated all image and script links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,12 +8,12 @@ export const Portfolio = () => {
         <section className="portfolio-section" id="portfolio">
             <SectionTitle title="PORTFOLIO" />
             <div className="row website-container">
-                <WebPage src="/images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
-                <WebPage src="/images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
-                <WebPage src="/images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
-                <WebPage src="/images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
-                <WebPage src="/images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
-                <WebPage src="/images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
+                <WebPage src="./images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
+                <WebPage src="./images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
+                <WebPage src="./images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
+                <WebPage src="./images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
+                <WebPage src="./images/chatbot.png" alt="chatbot" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap", "fab fa-node-js"]} />
+                <WebPage src="./images/easybank.png" alt="easybank" iconClasses={["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-bootstrap"]} />
             </div>
 
         </section>
